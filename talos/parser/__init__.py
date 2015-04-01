5b978666168928f9942039093e9d0a567814d809
Bryan Oakley <bryanoakley@echo.com>
2015-04-01 10:31:57 -0500

    first checking of parser

---
+++ talos/parser/__init__.py
@@ -0,0 +1,5 @@
+from parser import ResourceFile, SuiteFile, RobotFactory, TestcaseTable, KeywordTable
+from tables import DefaultTable, SettingTable, UnknownTable, VariableTable, MetadataTable, RobotTable
+from common import Row, Statement
+from testcase import Testcase
+from rfkeyword import Keyword
