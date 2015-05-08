3650bc6970dc65da0dc65aa7fd692136dff36ceb
Bryan Oakley <bryanoakley@echo.com>
2015-05-08 14:19:55 -0500

    more admin improvements

--- talos/admin.py
+++ talos/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
-from talos.models import Project, Collection, Keyword
+from talos.models import (Project, 
+                          ResourceFile, LibraryFile, SuiteFile, 
+                          Keyword, Testcase)
+
+class ResourceFileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['path', 'name']}),
+        ("Documentation", {'fields': ['doc_format', 'doc']}),
+    ]
+    list_display = ('name', 'path')
+    search_fields = ['path']
+
+class LibraryFileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['path', 'name']}),
+        ("Documentation", {'fields': ['doc_format', 'doc']}),
+        ("Library Information", {'fields': ['version', 'scope', 'namedargs']})
+    ]
+    list_display = ('name', 'path')
+    search_fields = ['path']
+
+class SuiteFileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'path', 'doc']}),
+    ]
+    list_display = ('name', 'path')
+    search_fields = ['path']
 
 admin.site.register(Project)
-admin.site.register(Collection)
+admin.site.register(ResourceFile, ResourceFileAdmin)
+admin.site.register(LibraryFile, LibraryFileAdmin)
+admin.site.register(SuiteFile, SuiteFileAdmin)
 admin.site.register(Keyword)
+admin.site.register(Testcase)
+
