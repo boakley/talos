badf5bbb889139e4efe9fd97d39267b26c96e14e
Bryan Oakley <bryanoakley@echo.com>
2015-05-08 14:19:04 -0500

    weak first attempt at custom templates

--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,22 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
